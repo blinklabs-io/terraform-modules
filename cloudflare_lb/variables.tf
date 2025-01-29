5fe8da138aa4580977c4e6a6c484effcb47354e5
Chris Gianelloni <wolf31o2@blinklabs.io>
2025-01-29 18:47:44 -0500

    feat: cloudflare_lb module (#6)
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

---
+++ cloudflare_lb/variables.tf
@@ -0,0 +1,60 @@
+variable "name" {
+  type = string
+}
+variable "account_id" {
+  type = string
+}
+variable "default_pool_ids" {
+  default = null
+}
+variable "fallback_pool_id" {
+  default = null
+}
+variable "load_balancer_monitor_id" {
+  default = null
+}
+variable "monitor_enabled" {
+  type    = bool
+  default = true
+}
+variable "monitor_expected_codes" {
+  type    = string
+  default = "200"
+}
+variable "monitor_headers" {
+  default = {}
+}
+variable "monitor_interval" {
+  type    = number
+  default = 60
+}
+variable "monitor_method" {
+  type    = string
+  default = "GET"
+}
+variable "monitor_path" {
+  type    = string
+  default = "/"
+}
+variable "monitor_retries" {
+  type    = number
+  default = 3
+}
+variable "monitor_timeout" {
+  type    = number
+  default = 5
+}
+variable "monitor_type" {
+  type    = string
+  default = "http"
+}
+variable "origins" {
+  type = object({
+    name    = string
+    address = string
+  })
+  default = {}
+}
+variable "zone_name" {
+  type = string
+}
