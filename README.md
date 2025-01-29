be9a854943a0877156faf1bccc26c36e0c93b44b
Chris Gianelloni <wolf31o2@blinklabs.io>
2025-01-29 11:11:45 -0500

    feat: cloudflare_page module
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ A highly-opinionated set of Terraform modules
 
 ## Cloudflare
 
-- `cloudflare_zone` configures a Cloudflare zone
+- `cloudflare_zone` configures a Cloudflare Zone
+- `cloudflare_page` configures a Cloudflare Pages project attached to GitHub
---
+++ cloudflare_page/variables.tf
@@ -0,0 +1,43 @@
+variable "account_id" {
+  type = string
+}
+variable "build_command" {
+  type    = string
+  default = "npm run build"
+}
+variable "deployment_variables" {
+  type    = map(any)
+  default = {}
+}
+variable "destination_dir" {
+  type    = string
+  default = "out"
+}
+variable "domains" {
+  type    = list(string)
+  default = []
+}
+variable "owner" {
+  type = string
+}
+variable "page_name" {
+  type = string
+}
+variable "preview_deployment_variables" {
+  type    = map(any)
+  default = {}
+}
+variable "production_branch" {
+  type    = string
+  default = "main"
+}
+variable "repo_name" {
+  type = string
+}
+variable "root_dir" {
+  type    = string
+  default = ""
+}
+variable "zone_name" {
+  type = string
+}
