49db93ead57506f039c72d0c390c00250a1ccf3f
Chris Gianelloni <wolf31o2@blinklabs.io>
2025-02-12 17:35:05 -0500

    feat: cloudflare_ruleset module (#7)
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

---
+++ cloudflare_ruleset/README.md
@@ -0,0 +1,13 @@
+# cloudflare_ruleset
+
+Requirements:
+- `cloudflare` provider
+
+Required variables:
+- `account_id`
+- `kind`
+- `name`
+- `phase`
+- `status_code`
+- `target_url`
+- `zone_name`
---
+++ cloudflare_ruleset/main.tf
@@ -0,0 +1,26 @@
+resource "cloudflare_ruleset" "this" {
+  account_id = var.account_id
+  name       = var.name
+  kind       = var.kind
+  phase      = var.phase
+  zone_id = data.cloudflare_zone.this.id
+
+  rules {
+    action = "redirect"
+    action_parameters {
+      from_value {
+        status_code = var.status_code
+        target_url {
+          value = var.target_url
+        }
+        preserve_query_string = false
+      }
+    }
+    expression = "(http.host eq \"${var.name}\")"
+    enabled = true
+  }
+}
+
+data "cloudflare_zone" "this" {
+  name = var.zone_name
+}
