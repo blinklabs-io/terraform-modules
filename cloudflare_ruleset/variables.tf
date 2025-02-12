c54f0a070c6dbb49f28184dcc9ac69cf6b6d6d71
Chris Gianelloni <wolf31o2@blinklabs.io>
2025-02-12 16:26:12 -0500

    feat: cloudflare_ruleset module
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

---
+++ cloudflare_ruleset/variables.tf
@@ -0,0 +1,18 @@
+variable "kind" {
+  type = string
+}
+variable "name" {
+  type = string
+}
+variable "phase" {
+  type = string
+}
+variable "status_code" {
+  type = number
+}
+variable "target_url" {
+  type = string
+}
+variable "zone_name" {
+  type = string
+}
