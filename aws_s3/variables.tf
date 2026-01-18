065fcfbf19b178f9045f1a3855303bbad51fcd36
Chris Gianelloni <wolf31o2@blinklabs.io>
2026-01-18 13:50:57 -0500

    chore(aws): formatting fixes
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

--- aws_s3/variables.tf
+++ aws_s3/variables.tf
@@ -2,16 +2,16 @@ variable "buckets" {
   description = "List of S3 buckets to manage"
   type = list(object({
     name = string
-    acl = optional(string)
+    acl  = optional(string)
     tags = optional(map(any))
     policy = optional(object({
       statements = list(object({
-        sid = optional(string)
-        effect = optional(string, "Allow")
-        actions = optional(list(string), [])
+        sid       = optional(string)
+        effect    = optional(string, "Allow")
+        actions   = optional(list(string), [])
         resources = optional(list(string))
         principals = optional(list(object({
-          type = string
+          type        = string
           identifiers = list(string)
         })), [])
       }))
