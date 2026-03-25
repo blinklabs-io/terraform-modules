34ea5c9ffab7afbd2fa6b544c1e04d0e54f59522
Tripura Repalle <tripura@blinklabs.io>
2026-03-25 15:05:46 -0400

    feat(add-iam-irsa-role): add iam irsa role
    Signed-off-by: Tripura Repalle <tripura@blinklabs.io>

--- aws_eks/iam.tf
+++ aws_eks/iam.tf
@@ -27,10 +27,13 @@ module "external_dns_irsa" {
 
   attach_external_dns_policy = true
 
+  // We need StringLike to use * in the namespace_service_accounts
+  trust_condition_test = "StringLike"
+
   oidc_providers = {
     main = {
       provider_arn               = module.eks.oidc_provider_arn
-      namespace_service_accounts = ["kube-system:external-dns"]
+      namespace_service_accounts = ["kube-system:*"]
     }
   }
 
