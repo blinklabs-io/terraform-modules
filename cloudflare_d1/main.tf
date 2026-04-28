04c5ac7507f598e6d9d188e49b206b24e4fcaa7e
Tripura Repalle <tripura@blinklabs.io>
2026-04-28 10:49:17 -0400

    chore: update cloudflare_d1 terraform for v5
    Signed-off-by: Tripura Repalle <tripura@blinklabs.io>

--- cloudflare_d1/main.tf
+++ cloudflare_d1/main.tf
@@ -2,4 +2,5 @@ resource "cloudflare_d1_database" "this" {
   account_id            = var.account_id
   name                  = var.name
   primary_location_hint = var.primary_location_hint
+  read_replication      = var.read_replication
 }
